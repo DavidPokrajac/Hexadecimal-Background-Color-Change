eaca69cbc8d1c4908e2d54612652892e87e7a865
DavidPokrajac <d0pokrajac@gmail.com>
2020-10-06 22:00:31 +0200

    Added JS functionality with for loop

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,31 @@
 window.onload = function(){
 
-  console.log("Testing");
+  // referencing span tag
+  var span = document.querySelector("span");
+
+  // referencing button tag
+  var button = document.querySelector("button");
+
+  // referencing body tag
+  var body = document.querySelector("body");
+
+  // defining hexadecimal values by one in an array
+  var hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+
+  // making an event listener for click event
+  button.addEventListener("click", function(){
+    var hex = "#";
+    for(var i = 0; i < 6; i++){
+      // defining hexcolorindex as the index of random item in hexValues array
+      var hexColorIndex = Math.floor(Math.random() * hexValues.length);
+
+      // appending iz to hex variable 6 times, as long as i is less than 6
+      hex += hexValues[hexColorIndex];
+    }
+
+    span.textContent = hex;
+    body.style.backgroundColor = hex;
+  });
+
 
 }
